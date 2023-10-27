a94933ed12cae7cc57cb2c31e9cf1b5c1a26a5eb
derian morgan <derianc@gmail.com>
2023-10-26 21:25:48 -0400

    add extensions

---
+++ BarBuddy.Extensions/Helpers.cs
@@ -0,0 +1,22 @@
+﻿namespace BarBuddy.Extensions
+{
+    public static class Helpers
+    {
+        public static DateTime RandomDOB(this DateTime date)
+        {
+            Random random = new Random();
+            DateTime start = new DateTime(2002, 1, 1);
+            int range = (DateTime.Today - start).Days;
+
+            int randomHour = random.Next(0, 24);
+            int randomMinute = random.Next(0, 60);
+            int randomSecond = random.Next(0, 60);
+
+            var randomDate = start.AddDays(random.Next(range));
+
+            var newDate = new DateTime(randomDate.Year, randomDate.Month, randomDate.Day, randomHour, randomMinute, randomSecond);
+
+            return newDate;
+        }
+    }
+}
